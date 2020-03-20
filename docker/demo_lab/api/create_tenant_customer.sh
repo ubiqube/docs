c34b3b21effb9f06c4c23ec8c9e2c2477e6ab80e
antoine-brun <antoine.brun@gmail.com>
2020-03-20 16:18:43 +0100

    ddmsa + demo_lab first version

--- docker/demo_lab/api/create_tenant_customer.sh
+++ docker/demo_lab/api/create_tenant_customer.sh
@@ -4,13 +4,17 @@ USER="ncroot"
 PASSWORD="ubiqube"
 OPERATOR="BLR"
 
+RESPONSE=`curl -H 'Content-Type: application/json' -XPOST http://127.0.0.1/ubi-api-rest/auth/token -d '{"username":"ncroot", "password":"ubiqube" }'`
+TOKEN=$(php -r 'echo json_decode($argv[1])->token;' "$RESPONSE")
+echo "$TOKEN" # Use for further processsing
+
 echo "--------------------------------------------------"
 echo "CREATE $OPERATOR TENANT AND Tyrell CUSTOMER"  
 echo "--------------------------------------------------"
 
-curl -u $USER:$PASSWORD -XPOST http://127.0.0.1/ubi-api-rest/operator/$OPERATOR?name=BladeRunner
-curl -u $USER:$PASSWORD -XPOST -H "Content-Type:application/json" -d'{"name":"Tyrell"}' http://127.0.0.1/ubi-api-rest/customer/{$OPERATOR}?name=Tyrell&reference=TyrellCorp
-curl -u $USER:$PASSWORD -XPOST -H "Content-Type:application/json" http://127.0.0.1/ubi-api-rest/repository/operator?uri=Process/$OPERATOR
-curl -u $USER:$PASSWORD -XPOST -H "Content-Type:application/json" http://127.0.0.1/ubi-api-rest/repository/operator?uri=CommandDefinition/$OPERATOR
+curl -H "Content-Type:application/json"  -H "Authorization: Bearer "$TOKEN -XPOST http://127.0.0.1/ubi-api-rest/operator/$OPERATOR?name=BladeRunner
+curl -H "Content-Type:application/json"  -H "Authorization: Bearer "$TOKEN -d '{"name":"Tyrell"}' http://127.0.0.1/ubi-api-rest/customer/$OPERATOR?name=Tyrell&reference=TyrellCorp
+curl -H "Content-Type:application/json"  -H "Authorization: Bearer "$TOKEN http://127.0.0.1/ubi-api-rest/repository/operator?uri=Process/$OPERATOR
+curl -H "Content-Type:application/json"  -H "Authorization: Bearer "$TOKEN http://127.0.0.1/ubi-api-rest/repository/operator?uri=CommandDefinition/$OPERATOR
 
 
